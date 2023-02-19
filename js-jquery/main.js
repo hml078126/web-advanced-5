af9d74fdb655b00906239814f6f1e7b037799e99
hml95 <htetmyet.lwin@mo.com.mm>
2023-02-19 15:03:07 +0630

    uploaded js test file

---
+++ js-jquery/main.js
@@ -0,0 +1,10 @@
+// var car = "Honda";
+// var car = "Toyota";
+
+// var x = 10;
+// var y = 5;
+
+// var cars = ["Honda", "Toyota", "Kia"];
+// cars[3] = "Nissan";
+
+var people = ["HML", "WKP", "MMB", "WYMT"];
